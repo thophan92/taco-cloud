ed3061d7969d88fe7ce9c47f6933f0c98a615c40
ThoPhan <38677422+thophanmis@users.noreply.github.com>
2020-09-02 14:05:55 +0900

    chap 2

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,7 +1,7 @@
 package tacos;
 
 import java.util.List;
-import java.util.ListResourceBundle;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -11,10 +11,9 @@ import lombok.Data;
 public class Taco {
 	
 	@NotNull
-	@Size(min = 5, message = "Name must be at least 5 characters long")
+	@Size(min=5, message="Name must be at least 5 characters long")
 	private String name;
 	
-	@Size(min = 1, message = "You must choose at least 1 ingredient")
-	private List<String> ingredient;
-
+	@Size(min=1, message="You must chose at least 1 ingredient")
+	private List<String> ingredients;
 }
---
+++ src/main/java/tacos/web/OrderController.java
@@ -0,0 +1,36 @@
+package tacos.web;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+import tacos.Order;
+import tacos.Taco;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+	@GetMapping("/current")
+	public String orderForm(Model model) {
+		model.addAttribute("order", new Order());
+		return "orderForm";
+	}
+
+	@PostMapping
+	public String processDesign(@Valid Taco design, Errors errors) {
+		if (errors.hasErrors()) {
+			return "design";
+		}
+		
+		log.info("Processing design: " + design);
+		return "redirect:/orders/current";
+	}
+}
